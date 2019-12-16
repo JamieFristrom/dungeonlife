2b30771a91ff4a371ddb678d7087ac71d0d07f71
JamieFristrom <jdfristrom@gmail.com>
2019-12-15 22:32:03 -0800

    different animation ids partially supported

---
+++ src/ReplicatedStorage/TS/AnimationManifest.ts
@@ -0,0 +1,21 @@
+import { ReplicatedStorage } from "@rbxts/services"
+import { DebugXL } from "./DebugXLTS"
+
+/**
+    Wrapper around data about what the animation id's are for this particular place or group.
+    I thought about having AnimationManifest be a data file where we'd keep track of our animation ids, 
+    which might have been a little easier to deal with, but then there'd be issues with checking in the file:
+    and if we left it in .gitignore what if we added more animation types later?
+*/
+export namespace AnimationManifest
+{
+    const animationManifest = ReplicatedStorage.WaitForChild( 'AnimationManifest', 5 )
+    DebugXL.Assert( animationManifest !== undefined )
+
+    export function getAnimInstance( animName: string ) : Animation
+    {
+        const animInstance = animationManifest ? animationManifest.WaitForChild<Animation>( animName, 5 ) : undefined
+        DebugXL.Assert( animInstance !== undefined )
+        return animInstance!  // because if it's not you're doing it wrong
+    }
+}
