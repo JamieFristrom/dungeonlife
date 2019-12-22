09bafc5fb685eae77965e2530b63296210177ec0
JamieFristrom <jdfristrom@gmail.com>
2019-12-22 12:38:02 -0800

    Hardcoded dev/cheat whitelist for non-group games

--- src/ReplicatedStorage/TS/CheatUtility.ts
+++ src/ReplicatedStorage/TS/CheatUtility.ts
@@ -1,10 +1,14 @@
 import { PlacesManifest } from "./PlacesManifest"
+import { Whitelist } from "./Whitelist"
 
 class CheatUtilityClass
 {
 	// use : calling from Lua
     PlayerWhitelisted( player: Player )
     {
+        if( Whitelist.whitelist.find( (value)=> value === player.UserId ) )
+            return true;
+
         let rank = 0
         let [ success, msg ] = pcall( function() { rank = player.GetRankInGroup( PlacesManifest.getCurrentGame().groupId ) } )
 	    if( !success )
