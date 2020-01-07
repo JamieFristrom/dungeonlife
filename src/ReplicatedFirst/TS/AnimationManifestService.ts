c0a0ba47083a8bb3d94b646e6b2b9fed6bceb2eb
JamieFristrom <jdfristrom@gmail.com>
2020-01-07 08:01:57 -0800

    Animation manifest do-over

---
+++ src/ReplicatedFirst/TS/AnimationManifestService.ts
@@ -0,0 +1,38 @@
+import { ReplicatedStorage, ReplicatedFirst } from "@rbxts/services"
+import { DebugXL } from "../../ReplicatedStorage/TS/DebugXLTS"
+import { AnimationManifest } from "./AnimationManifest"
+/**
+    \brief Wrapper around data about what the animation id's are for this particular place or group.
+    I went back and forth on how this works, between having it in a ts file vs having it be a folder full of animations
+    in the RBXL. Having it be in the RBXL turned out to be a mistake because then I couldn't make changes to the RBXL
+    without bashing every other place's animation manifest.
+*/
+export namespace AnimationManifestService
+{
+    const animationFolder = ReplicatedFirst.WaitForChild( 'AnimationManifest', 5 )
+    DebugXL.Assert( animationFolder !== undefined )
+
+    export function getAnimInstance( animName: string ) : Animation
+    {
+        let animInstance = animationFolder!.FindFirstChild<Animation>( animName )
+        if( !animInstance ) {
+            const animId = AnimationManifest[animName]
+            if( !animId )
+            {
+                DebugXL.Error('Animation '+animName+' not in manifest:')
+                DebugXL.Dump(AnimationManifest)
+            }
+            else
+            {
+                // A bit weird - if we're running on the server this will automatically replicate to the client.
+                // So the client can get updated one of two ways, either it runs this code first or the server replicates first.
+                // Either way should be fine.
+                animInstance = new Instance("Animation")
+                animInstance.Name = animName
+                animInstance.AnimationId = AnimationManifest[animName]
+                animInstance.Parent = animationFolder
+            }
+        }
+        return animInstance!
+    }
+}
