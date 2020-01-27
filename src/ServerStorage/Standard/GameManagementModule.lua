53626ca2295e09e5832d20b9eb73508be71aaa67
Jamie_Fristrom <jdfristrom@gmail.com>
2020-01-26 19:21:47 -0800

    new static analysis fixes started

--- src/ServerStorage/Standard/GameManagementModule.lua
+++ src/ServerStorage/Standard/GameManagementModule.lua
@@ -149,29 +149,9 @@ GameManagement.levelStartTime = time()
 --	badTbl[ badRef ] = "bad thing"
 --end )
 
--- hack
-local GameManagementRemote = {}
-
-function GameManagementRemote.AcknowledgeGuiLoaded( player )
-	DungeonPlayer:Get( player ).guiLoadedB = true
-
-	--print( player.Name.." gui acknowledged" )
-end
-
-workspace.Signals.GameManagementRE.OnServerEvent:Connect( function( player, funcName, ... )
-	GameManagementRemote[ funcName ]( player, ... )
-end)
-warn( "Time until GameManagementRE connected: "..time() )
-
-workspace.Signals.ChooseHeroRE.OnServerEvent:Connect( function( player, code )
-	DebugXL:Assert( code == "ack")
-	print( player.Name.." ChooseHeroRE acknowledged" )
-	DungeonPlayer:Get( player ).chooseHeroREAckedB = true
-end )
-
 
 -- dungeon player class
-DungeonPlayer = {
+local DungeonPlayer = {
 	
 }
 
@@ -196,6 +176,28 @@ function DungeonPlayer:Get( player )
 	end 
 	return dungeonPlayersT[ player ] 
 end
+
+
+-- hack
+local GameManagementRemote = {}
+
+function GameManagementRemote.AcknowledgeGuiLoaded( player )
+	DungeonPlayer:Get( player ).guiLoadedB = true
+
+	--print( player.Name.." gui acknowledged" )
+end
+
+workspace.Signals.GameManagementRE.OnServerEvent:Connect( function( player, funcName, ... )
+	GameManagementRemote[ funcName ]( player, ... )
+end)
+warn( "Time until GameManagementRE connected: "..time() )
+
+workspace.Signals.ChooseHeroRE.OnServerEvent:Connect( function( player, code )
+	DebugXL:Assert( code == "ack")
+	print( player.Name.." ChooseHeroRE acknowledged" )
+	DungeonPlayer:Get( player ).chooseHeroREAckedB = true
+end )
+
 
 -- utility
 
