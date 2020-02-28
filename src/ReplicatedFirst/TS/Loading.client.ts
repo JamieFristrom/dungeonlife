3467b74b47d6e80f26d2fd849e783f8f433a24f9
JamieFristrom <jdfristrom@gmail.com>
2020-02-27 18:36:56 -0800

    robust to missing shop from old save game

--- src/ReplicatedFirst/TS/Loading.client.ts
+++ src/ReplicatedFirst/TS/Loading.client.ts
@@ -1,7 +1,7 @@
 
 import { Workspace, RunService } from "@rbxts/services"
-warn( "DUNGEON LIFE 2/23/2020" )
-warn( "Refactor plus small fixes plus fixed chests" )
+warn( "DUNGEON LIFE 2/27/2020" )
+warn( "Fix shop loading issues" )
 
 let loadingStructure = script.Parent!.Parent!.WaitForChild<Part>("LoadingStructure")
 let waitingImage = loadingStructure.WaitForChild("BillboardGui").WaitForChild("LogoFrame").WaitForChild<ImageLabel>("WaitingImage")
