5571669059db150425d0408240892e8a3a3155dd
Jamie Fristrom <jafristr@microsoft.com>
2020-05-25 16:55:59 -0700

    Moved LootServer to typescript

--- src/ServerStorage/Standard/HeroesModule.d.ts
+++ src/ServerStorage/Standard/HeroesModule.d.ts
@@ -1,13 +1,14 @@
 import { HeroStable } from "ReplicatedStorage/TS/HeroStableTS";
 import { FlexTool } from "ReplicatedStorage/TS/FlexToolTS";
+import { Hero } from "ReplicatedStorage/TS/HeroTS"
 
 declare class HeroesModule
 {
     GetSavedPlayerCharactersWait( player: Player ) : HeroStable
-    
     AdjustGold( player: Player, amount: number, analyticsItemIdS: string, analyticsItemTypeS: string ) : void
-
+    SaveHeroesWait( player: Player ) : void
     RecordTool( player: Player, flexToolInst: FlexTool ) : void
+    GetPCDataWait( player: Player ) : Hero
 }
 
 declare let heroesModule: HeroesModule
