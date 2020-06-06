f3fd6ef28c8cca214ca03c3c324aecc54d543bec
JamieFristrom <jdfristrom@gmail.com>
2020-06-06 12:17:09 -0700

    Mana potions were invisible for heroes; fixed

--- src/ServerStorage/TS/FlexibleToolsServer.ts
+++ src/ServerStorage/TS/FlexibleToolsServer.ts
@@ -1,18 +1,18 @@
 import { ServerStorage, Players } from '@rbxts/services';
 
 import { DebugXL } from 'ReplicatedStorage/TS/DebugXLTS';
-DebugXL.logI( script.Name, 'Executed' )
+DebugXL.logI('Executed', script.Name)
+
 import { FlexTool } from 'ReplicatedStorage/TS/FlexToolTS'
 import { ToolData } from 'ReplicatedStorage/TS/ToolDataTS';
-DebugXL.logD( script.Name, 'FlexibleToolServer: ReplicatedStorage/TS imports succesful')
+DebugXL.logD('Requires', 'FlexibleToolServer: ReplicatedStorage/TS imports succesful')
 
-import { Enhancements } from 'ReplicatedStorage/TS/EnhancementsTS';
 import { ValueHelper } from 'ReplicatedStorage/TS/ValueHelper';
 
 import { CreateToolParamsI } from 'ServerStorage/TS/CreateToolParamsI'
 
 import * as PossessionData from 'ReplicatedStorage/Standard/PossessionDataStd';
-DebugXL.logD( script.Name, 'FlexibleToolServer: imports succesful')
+DebugXL.logD('Requires', 'FlexibleToolServer: imports succesful')
 
 
 // thinking about things which can hold weapons
@@ -20,167 +20,139 @@ DebugXL.logD( script.Name, 'FlexibleToolServer: imports succesful')
 // currently, players hold weapons
 // what about the shop? how does that work?
 // heroes have shops
-const ToolsFolder = ServerStorage.WaitForChild<Folder>('Tools',10)!
-DebugXL.Assert( ToolsFolder !== undefined )
+const ToolsFolder = ServerStorage.WaitForChild<Folder>('Tools', 10)!
+DebugXL.Assert(ToolsFolder !== undefined)
 
 type Character = Model
 
-export interface FlexToolAccessor 
-{
+export interface FlexToolAccessor {
     flexToolInst: FlexTool,
     character: Character,            // this is what we need to broaden to include CPU players
     possessionsKey: string     // which tool in player's inventory it is
 }
 
 
-export namespace FlexibleToolsServer
-{
-    let mobToolCache : Folder = ServerStorage.FindFirstChild<Folder>('MobToolCache')!
-    DebugXL.Assert( mobToolCache !== undefined )
+export namespace FlexibleToolsServer {
+    let mobToolCache: Folder = ServerStorage.FindFirstChild<Folder>('MobToolCache')!
+    DebugXL.Assert(mobToolCache !== undefined)
 
     let serverToolDataT = new Map<number, FlexToolAccessor>()
 
-    export function getFlexToolAccessor( toolId : number ) 
-    {
-        return serverToolDataT.get( toolId );
+    export function getFlexToolAccessor(toolId: number) {
+        return serverToolDataT.get(toolId);
     }
 
-    export function setFlexToolInst( toolId: number, fta: FlexToolAccessor )
-    {
-        serverToolDataT.set( toolId, fta )
+    export function setFlexToolInst(toolId: number, fta: FlexToolAccessor) {
+        serverToolDataT.set(toolId, fta)
     }
-    
+
     let toolIdServer = 0
 
-    function serveToolId()
-    {
+    function serveToolId() {
         return toolIdServer++
     }
 
-    export function getFlexTool( toolObj: Tool )
-    {
+    export function getFlexTool(toolObj: Tool) {
         const toolId = toolObj.FindFirstChild<NumberValue>('ToolId')
-        if( !toolId )
-        {
-            DebugXL.logE( script.Name, `Can't find ToolId for ${toolObj.GetFullName()}`)
+        if (!toolId) {
+            DebugXL.logE(script.Name, `Can't find ToolId for ${toolObj.GetFullName()}`)
             return FlexTool.nullTool
         }
-        return getFlexToolFromId( toolId.Value )
+        return getFlexToolFromId(toolId.Value)
     }
 
-    export function getFlexToolFromId( toolId: number )
-    {
-        const fta = getFlexToolAccessor( toolId ) 
-        DebugXL.Assert( fta !== undefined )
-        if( fta ) 
+    export function getFlexToolFromId(toolId: number) {
+        const fta = getFlexToolAccessor(toolId)
+        DebugXL.Assert(fta !== undefined)
+        if (fta)
             return fta.flexToolInst
         return FlexTool.nullTool
     }
 
-    export function getToolBaseData( toolObj: Tool )
-    {
-        const flexTool = getFlexTool( toolObj )
-        return getFlexToolBaseData( flexTool )
+    export function getToolBaseData(toolObj: Tool) {
+        const flexTool = getFlexTool(toolObj)
+        return getFlexToolBaseData(flexTool)
     }
 
-    export function getFlexToolBaseData( flexTool: FlexTool )
-    {
-        return ToolData.dataT[ flexTool.baseDataS ]
+    export function getFlexToolBaseData(flexTool: FlexTool) {
+        return ToolData.dataT[flexTool.baseDataS]
     }
 
-    export function createTool( params: CreateToolParamsI )
-    {
+    export function createTool(params: CreateToolParamsI) {
         const flexTool = params.toolInstanceDatumT
         const destinationCharacter = params.destinationCharacter
         const activeSkins = params.activeSkinsT
         const itemPoolKey = params.possessionsKey
 
         // validate parameters so calling from Lua is safer
-        DebugXL.Assert( destinationCharacter.IsA('Model') )
+        DebugXL.Assert(destinationCharacter.IsA('Model'))
 
-        DebugXL.logI( script.Name, `Creating ${flexTool.baseDataS} for ${destinationCharacter.Name}` )
-        
+        DebugXL.logI(script.Name, `Creating ${flexTool.baseDataS} for ${destinationCharacter.Name}`)
+
         const toolId = serveToolId()
-        FlexibleToolsServer.setFlexToolInst( toolId, { flexToolInst: flexTool, character: destinationCharacter, possessionsKey: itemPoolKey })
-        
+        FlexibleToolsServer.setFlexToolInst(toolId, { flexToolInst: flexTool, character: destinationCharacter, possessionsKey: itemPoolKey })
+
         // if tool doesn't have enhancements add an empty array so we don't have to constantly check if enhancementsA is nil
         if (!flexTool.enhancementsA) flexTool.enhancementsA = []
-        
-        const toolBaseDatum = ToolData.dataT[ flexTool.baseDataS ]
-        if (!toolBaseDatum) DebugXL.logE( script.Name, `Unable to find possession ${flexTool.baseDataS}` ) 
-        
+
+        const toolBaseDatum = ToolData.dataT[flexTool.baseDataS]
+        if (!toolBaseDatum) DebugXL.logE(script.Name, `Unable to find possession ${flexTool.baseDataS}`)
+
         let baseToolId = toolBaseDatum.baseToolS
-        DebugXL.Assert( baseToolId !== undefined )
-        if( baseToolId )
-        {
+        DebugXL.Assert(baseToolId !== undefined)
+        if (baseToolId) {
             let textureSwapId = undefined
-            if (!toolBaseDatum.skinType) 
-            {
-                DebugXL.logE( script.Name, `${toolBaseDatum.idS} has no skinType`)
+            if (!toolBaseDatum.skinType) {
+                DebugXL.logE(script.Name, `${toolBaseDatum.idS} has no skinType`)
             }
-            else
-            {
-                if( activeSkins[ toolBaseDatum.skinType ])
-                {
-                    const reskin = PossessionData.dataT[ activeSkins[ toolBaseDatum.skinType ]] as PossessionData.SkinDatumI
-                    if( reskin )
-                    {
+            else {
+                if (activeSkins[toolBaseDatum.skinType]) {
+                    const reskin = PossessionData.dataT[activeSkins[toolBaseDatum.skinType]] as PossessionData.SkinDatumI
+                    if (reskin) {
                         baseToolId = reskin.baseToolS
                         textureSwapId = reskin.textureSwapId
                     }
                 }
             }
-             
-            const toolTemplate = ToolsFolder.FindFirstChild<Tool>( baseToolId )
-            DebugXL.Assert( toolTemplate !== undefined )
-            if( toolTemplate )
-            {
+
+            const toolTemplate = ToolsFolder.FindFirstChild<Tool>(baseToolId)
+            DebugXL.Assert(toolTemplate !== undefined)
+            if (toolTemplate) {
                 const newToolInstance = toolTemplate.Clone() as Tool
-                FlexTool.retexture( newToolInstance, textureSwapId )
+                FlexTool.retexture(newToolInstance, textureSwapId)
                 let nonDefaultFX = false
-                for( let i=0; i<flexTool.enhancementsA.size(); i++ )
-                {
+                for (let i = 0; i < flexTool.enhancementsA.size(); i++) {
                     const enhancement = flexTool.enhancementsA[i]
 
                     // enable enhancement related effects
-                    for( let descendent of newToolInstance.GetDescendants() )
-                    {
-                        if( descendent.Name === 'FX'+enhancement.flavorS )
-                        {
-                            if( descendent.IsA('Script') )
-                            {
+                    for (let descendent of newToolInstance.GetDescendants()) {
+                        if (descendent.Name === 'FX' + enhancement.flavorS) {
+                            if (descendent.IsA('Script')) {
                                 descendent.Disabled = false
                             }
-                            else if( descendent.IsA('ParticleEmitter') || descendent.IsA('Beam') || descendent.IsA('Light') || descendent.IsA('Fire') || descendent.IsA('Trail'))
-                            {
+                            else if (descendent.IsA('ParticleEmitter') || descendent.IsA('Beam') || descendent.IsA('Light') || descendent.IsA('Fire') || descendent.IsA('Trail')) {
                                 // now *that's* a new idea to me. Any of these things have Enabled properties, so...
                                 const descendentEmitter = descendent as ParticleEmitter | Beam | Light | Fire | Trail
                                 descendentEmitter.Enabled = true
                                 nonDefaultFX = true
                             }
-                            else
-                            {
-                                DebugXL.logE( script.Name, `Unsupported enhancement fx type ${descendent.ClassName} on ${descendent.GetFullName()}` )
+                            else {
+                                DebugXL.logE(script.Name, `Unsupported enhancement fx type ${descendent.ClassName} on ${descendent.GetFullName()}`)
                             }
                         }
                     }
                 }
 
-                if( nonDefaultFX )
-                {
+                if (nonDefaultFX) {
                     // remove default effects
-                    for( let descendent of newToolInstance.GetDescendants() )
-                    {
-                        if( descendent.Name === 'FXdefault' )
-                        {
-                            if( descendent.IsA('ParticleEmitter') || descendent.IsA('Beam') || descendent.IsA('Light') || descendent.IsA('Fire') || descendent.IsA('Trail'))
-                            {
+                    for (let descendent of newToolInstance.GetDescendants()) {
+                        if (descendent.Name === 'FXdefault') {
+                            if (descendent.IsA('ParticleEmitter') || descendent.IsA('Beam') || descendent.IsA('Light') || descendent.IsA('Fire') || descendent.IsA('Trail')) {
                                 let effect = descendent as ParticleEmitter | Beam | Light | Fire | Trail
                                 effect.Enabled = false
                             }
-                            else
-                            {
-                                DebugXL.logE( script.Name, `Unsupported enhancement fx type ${descendent.ClassName} on ${descendent.GetFullName()}` )
+                            else {
+                                DebugXL.logE(script.Name, `Unsupported enhancement fx type ${descendent.ClassName} on ${descendent.GetFullName()}`)
                             }
                             // delibaretely not enabling something already enabled because I think there's a perf hit
                             // and who knows, there may be tools with disabled default fx that were there for temp or testing
@@ -189,31 +161,29 @@ export namespace FlexibleToolsServer
                 }
 
                 newToolInstance.CanBeDropped = false
-        
+
                 // attach tool id to tool 
-                ValueHelper.AddNumberValue( newToolInstance, 'ToolId', toolId )
+                ValueHelper.AddNumberValue(newToolInstance, 'ToolId', toolId)
 
                 // here's some data we attach to the tool itself to make it easy to look up on the client
                 // attach inventory slot so we can find it on the client
-                ValueHelper.AddStringValue( newToolInstance, 'PossessionKey', itemPoolKey )
+                ValueHelper.AddStringValue(newToolInstance, 'PossessionKey', itemPoolKey)
 
                 // we'll need to be able to adjust these for heroes with buffs. fixme
-                if( toolBaseDatum.rangeN )  // unintuitively, ranged weapons are the weapons that *don't* have range - their range is infinite
-                    ValueHelper.AddNumberValue( newToolInstance, 'Range', toolBaseDatum.rangeN )
+                if (toolBaseDatum.rangeN)  // unintuitively, ranged weapons are the weapons that *don't* have range - their range is infinite
+                    ValueHelper.AddNumberValue(newToolInstance, 'Range', toolBaseDatum.rangeN)
 
-                ValueHelper.AddNumberValue( newToolInstance, 'Cooldown', flexTool.getCooldown() )
-                ValueHelper.AddNumberValue( newToolInstance, 'ManaCost', flexTool.getManaCost() )
-                ValueHelper.AddNumberValue( newToolInstance, 'WalkSpeedMul', toolBaseDatum.walkSpeedMulN )
+                ValueHelper.AddNumberValue(newToolInstance, 'Cooldown', flexTool.getCooldown())
+                ValueHelper.AddNumberValue(newToolInstance, 'ManaCost', flexTool.getManaCost())
+                ValueHelper.AddNumberValue(newToolInstance, 'WalkSpeedMul', toolBaseDatum.walkSpeedMulN)
 
-                const destinationPlayer = Players.GetPlayerFromCharacter( destinationCharacter )
-                if( destinationPlayer )
-                {
+                const destinationPlayer = Players.GetPlayerFromCharacter(destinationCharacter)
+                if (destinationPlayer) {
                     // we're using Roblox's backpack as a holding space for tools to combat lag; if we equip a weapon that exists on
                     // the server
                     newToolInstance.Parent = destinationPlayer.FindFirstChild<Backpack>('Backpack')
                 }
-                else
-                {
+                else {
                     newToolInstance.Parent = mobToolCache
                 }
 
---
+++ src/StarterPlayer/StarterPlayerScripts/TS/PotionButtonTests.client.ts
@@ -0,0 +1,71 @@
+
+// Copyright (c) Happion Laboratories - see license at https://github.com/JamieFristrom/dungeonlife/blob/master/LICENSE.md
+
+import { DebugXL } from "ReplicatedStorage/TS/DebugXLTS";
+DebugXL.logI('Executed', script.Name)
+
+import { FlexTool } from "ReplicatedStorage/TS/FlexToolTS"
+import { PotionButton } from "ReplicatedStorage/TS/PotionButton"
+import { ReplicatedStorage, Players } from "@rbxts/services"
+import { Monster } from "ReplicatedStorage/TS/Monster"
+import { Hero } from "ReplicatedStorage/TS/HeroTS"
+
+type Character = Model
+
+const manaPotionFrame = Players.LocalPlayer!.WaitForChild<PlayerGui>("PlayerGui").WaitForChild<ScreenGui>("NoResetGui").WaitForChild<Frame>("PotionbarR")
+const manaPotionItemFrame = manaPotionFrame.WaitForChild<Frame>("Item")
+
+const testManaPotionButton = new PotionButton({
+    funcName: "TakeBestManaPotion",
+    itemName: "Mana",
+    warningFunc: (character: Character) => {
+        const manaValue = character.FindFirstChild<NumberValue>("ManaValue")
+        const maxManaValue = character.FindFirstChild<NumberValue>("MaxManaValue")
+        if (manaValue && maxManaValue && manaValue.Value < maxManaValue.Value * 0.33)
+            return true
+        return false
+    },
+    potionFrame: manaPotionFrame
+})
+
+const animationDummy = ReplicatedStorage.WaitForChild<Folder>("TestObjects").WaitForChild<Character>("AnimationDummy")
+
+const monsterRecord = new Monster("dummyMonster", [], 1)
+
+testManaPotionButton.refresh(animationDummy, monsterRecord)
+
+// one or the other is fine
+DebugXL.Assert(!manaPotionFrame.Visible || !manaPotionItemFrame.Visible)
+
+const heroWithPotRecord = new Hero("dummyHero", {
+    strN: 1,
+    dexN: 1,
+    willN: 1,
+    conN: 1,
+    experienceN: 1,
+    goldN: 1,
+    deepestDungeonLevelN: 1,
+    totalTimeN: 1,
+}, [new FlexTool("Mana", 1, [])])
+
+testManaPotionButton.refresh(animationDummy, heroWithPotRecord)
+
+DebugXL.Assert(manaPotionFrame.Visible)
+DebugXL.Assert(manaPotionItemFrame.Visible)
+
+const heroRecord = new Hero("dummyHero", {
+    strN: 1,
+    dexN: 1,
+    willN: 1,
+    conN: 1,
+    experienceN: 1,
+    goldN: 1,
+    deepestDungeonLevelN: 1,
+    totalTimeN: 1,
+}, [])
+
+testManaPotionButton.refresh(animationDummy, heroRecord)
+
+DebugXL.Assert(!manaPotionFrame.Visible || !manaPotionItemFrame.Visible)
+
+
