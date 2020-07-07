29c6d972f534496309f5674619840f7ca7dad767
JamieFristrom <jdfristrom@gmail.com>
2020-07-07 16:09:23 -0700

    Fix unanchored parts

--- src/ServerScriptService/TS/Test.server.ts
+++ src/ServerScriptService/TS/Test.server.ts
@@ -1,39 +1,51 @@
+
+// Copyright (c) Happion Laboratories - see license at https://github.com/JamieFristrom/dungeonlife/blob/master/LICENSE.md
+
+import { DebugXL } from 'ReplicatedStorage/TS/DebugXLTS'
+DebugXL.logI('Executed', script.GetFullName())
+
 import { RunService, Teams, Players, Workspace } from "@rbxts/services"
 
 import { CharacterServer } from "ServerStorage/TS/CharacterServer"
 import { CharacterRecord } from "ReplicatedStorage/TS/CharacterRecord"
 import { Hero } from "ReplicatedStorage/TS/HeroTS";
 import { Monster } from "ReplicatedStorage/TS/Monster"
-import { DebugXL } from "ReplicatedStorage/TS/DebugXLTS";
 import { MonsterServer } from "ServerStorage/TS/MonsterServer";
 
 import * as PossessionData from "ReplicatedStorage/Standard/PossessionDataStd"
 import { MonsterDatumI } from "ReplicatedStorage/TS/MonsterDatum";
 
-if( RunService.IsStudio())
-{
+if (RunService.IsStudio()) {
+    // make sure nothing unanchored
+    for (let descendant of Workspace.GetDescendants()) {
+        if (descendant.IsA("BasePart"))
+            if (!descendant.Anchored) {
+                DebugXL.logW("Parts", descendant.GetFullName() + " is not anchored")
+            }
+    }
+
     // place to put tests. why is it empty 
     let heroes = Teams.WaitForChild<Team>("Heroes")
 
     // just so we can step in debugger, not a test-case type test which would require creating fake players...  which we can totally do, huh.
     wait(1)
-    while( heroes.GetPlayers().size()===0 ) wait(1)
+    while (heroes.GetPlayers().size() === 0) wait(1)
 
     // test associating a hero with player
     let heroPlayer = Players.GetChildren()[0] as Player
     let fakePlayerMap = new Map<Player, CharacterRecord>()
-    fakePlayerMap.set( heroPlayer, new Hero( 'Warrior', 
+    fakePlayerMap.set(heroPlayer, new Hero('Warrior',
         { strN: 10, dexN: 10, conN: 10, willN: 10, experienceN: 0, goldN: 0, deepestDungeonLevelN: 0, totalTimeN: 0 },
-        [] ) )
-    DebugXL.Assert( fakePlayerMap.get( heroPlayer )!.getTeam() === heroes )
+        []))
+    DebugXL.Assert(fakePlayerMap.get(heroPlayer)!.getTeam() === heroes)
 
     // test associating a fake monster with player
     let fakeMonsterPlayer = Workspace.FindFirstChild('Camera') as Player
-    fakePlayerMap.set( fakeMonsterPlayer, new Monster( 'x',
+    fakePlayerMap.set(fakeMonsterPlayer, new Monster('x',
         [],
-        1 ) )
-    DebugXL.Assert( fakePlayerMap.get( heroPlayer )!.getTeam() === heroes )
-    DebugXL.Assert( fakePlayerMap.get( fakeMonsterPlayer )!.getTeam() === Teams.WaitForChild<Team>('Monsters') )
+        1))
+    DebugXL.Assert(fakePlayerMap.get(heroPlayer)!.getTeam() === heroes)
+    DebugXL.Assert(fakePlayerMap.get(fakeMonsterPlayer)!.getTeam() === Teams.WaitForChild<Team>('Monsters'))
     //let test0 = CharacterServer.IsDangerZoneForHero( fakePlayerMap, heroPlayer )
     //DebugXL.Assert( test0 === false )
 
