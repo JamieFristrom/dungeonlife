5c3b11489c53e89e6a063bbd8689dba284d22646
Jamie Fristrom <jdfristrom@gmail.com>
2020-07-18 15:25:15 -0700

    Fixed error when braziers are placed before level spawns spawners

--- src/ServerStorage/TS/Furnisher.ts
+++ src/ServerStorage/TS/Furnisher.ts
@@ -13,27 +13,30 @@ import { Workspace } from '@rbxts/services'
 const buildingFolder = Workspace.WaitForChild<Folder>("Building")
 
 export namespace Furnisher {
-    export function countFurnishingsOfType( furnishingType: PossessionData.FurnishingEnum ) {
-        // FIXME - this can somehow cause indexing nil
-        return buildingFolder.GetChildren().filter( (furnishing)=>{
-            if( !PossessionData.dataT[furnishing.Name]) {
-                DebugXL.Error( furnishing.Name+" doesn't exist in PossessionData")
-                return false
+    export function countFurnishingsOfType(furnishingType: PossessionData.FurnishingEnum) {
+        return buildingFolder.GetChildren().filter((furnishing) => {
+            const possessionNameObj = furnishing.FindFirstChild<StringValue>("PossessionName")
+            DebugXL.Assert(possessionNameObj !== undefined)
+            if (possessionNameObj) {
+                if (!PossessionData.dataT[possessionNameObj.Value]) {
+                    DebugXL.Error(furnishing.Name + " doesn't exist in PossessionData")
+                }
+                else {
+                    return PossessionData.dataT[furnishing.Name].furnishingType === furnishingType
+                }
             }
-            else {
-                return PossessionData.dataT[furnishing.Name].furnishingType===furnishingType
-            }
+            return false
         }).size()
     }
 
-    export function furnishWithFurnishingsOfType( availableBlueprints: Map<string,Boolean>, expectedTotal: number, furnishingType: PossessionData.FurnishingEnum ) {
-        const existingCount = countFurnishingsOfType( furnishingType )
-        if( existingCount < expectedTotal ) {
-            const bossBlueprints = availableBlueprints.keys().filter( 
-                (blueprint)=>PossessionData.dataT[blueprint].furnishingType === furnishingType );
-            const spawnData = bossBlueprints.map( (blueprint)=>PossessionData.dataT[blueprint] )
-            if( spawnData.size() >= 1 ) {
-                FurnishServer.PlaceSpawns( spawnData, expectedTotal-existingCount )
+    export function furnishWithFurnishingsOfType(availableBlueprints: Map<string, Boolean>, expectedTotal: number, furnishingType: PossessionData.FurnishingEnum) {
+        const existingCount = countFurnishingsOfType(furnishingType)
+        if (existingCount < expectedTotal) {
+            const bossBlueprints = availableBlueprints.keys().filter(
+                (blueprint) => PossessionData.dataT[blueprint].furnishingType === furnishingType);
+            const spawnData = bossBlueprints.map((blueprint) => PossessionData.dataT[blueprint])
+            if (spawnData.size() >= 1) {
+                FurnishServer.PlaceSpawns(spawnData, expectedTotal - existingCount)
                 return expectedTotal
             }
         }
@@ -43,8 +46,8 @@ export namespace Furnisher {
     // this gets called twice; once when the level is created and once when the heroes are ready in case the monsters
     // didn't place enough spawn points or a hero arrived late
     // so it checks to see if the right amount of spawns are there and adds more if necessary
-    export function furnishWithRandomSpawns( numHeroes: number ) {
-        const bossCount = furnishWithFurnishingsOfType( FloorData.CurrentFloor().availableBlueprintsT, 1, PossessionData.FurnishingEnum.BossSpawn )
-        furnishWithFurnishingsOfType( FloorData.CurrentFloor().availableBlueprintsT, numHeroes+3/(bossCount+1), PossessionData.FurnishingEnum.Spawn )
+    export function furnishWithRandomSpawns(numHeroes: number) {
+        const bossCount = furnishWithFurnishingsOfType(FloorData.CurrentFloor().availableBlueprintsT, 1, PossessionData.FurnishingEnum.BossSpawn)
+        furnishWithFurnishingsOfType(FloorData.CurrentFloor().availableBlueprintsT, numHeroes + 3 / (bossCount + 1), PossessionData.FurnishingEnum.Spawn)
     }
 }
